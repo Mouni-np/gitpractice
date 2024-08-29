d2a2f3a485aec3a9253f1e0f2cc21dc2f8a70f83
dheeraj <dheerajnp200019@gmail.com>
2024-08-29 20:28:34 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+person1=$1
+person2=$2
+echo "$person1 : hi $person2, how r u?"
+echo "$person2 : hi i am fine. what abt u?"
+echo "$person1 : I have started learning devops"
+echo "$person2 : thats nice"
