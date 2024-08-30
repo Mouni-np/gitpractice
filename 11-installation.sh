308d47b30828b509831f55b2058234a92a95ce99
dheeraj <dheerajnp200019@gmail.com>
2024-08-30 18:35:10 +0530

    conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,7 +9,7 @@ then
     exit 1
 fi
 
-dnf list installed git 
+dnf list installed git
 
 if [ $? -ne 0 ]
 then 
@@ -21,6 +21,23 @@ then
         exit 1
     else
         echo "git installation is success"
+    fi
+else
+    echo "already installed"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed, going to install"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "mysql installation is not success...check it.."
+        exit 1
+    else
+        echo "mysql installation is success"
     fi
 else
     echo "already installed"
