6929cd632e4597abcd4214004d99c0428e6b2211
dheeraj <dheerajnp200019@gmail.com>
2024-08-30 18:59:34 +0530

    functions

---
+++ 12-functions.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+userid=$(id -u)
+ validate(){
+    echo "exit status: $1"
+ }
+
+if [ $userid -ne 0 ]
+then 
+    echo "run script with root priveleges"
+    exit 1
+fi
+
+dnf list installed git
+
+validate $?
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "git is not installed, going to install"
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "git installation is not success...check it.."
+#         exit 1
+#     else
+#         echo "git installation is success"
+#     fi
+# else
+#     echo "already installed"
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "mysql is not installed, going to install"
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "mysql installation is not success...check it.."
+#         exit 1
+#     else
+#         echo "mysql installation is success"
+#     fi
+# else
+#     echo "already installed"
+# fi
