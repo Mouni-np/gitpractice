643271394a000b9182d97edea0cc075c9f6e8493
dheeraj <dheerajnp200019@gmail.com>
2024-08-30 11:10:20 +0530

    command

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -2,4 +2,5 @@
 echo "all variables pass to the script: $@"
 echo " no.of variables passed: $#"
 echo "Script name: $0"
-echo "current wd"$PWD"
+echo "current wd $PWD"
+echo "home dir of current user $HOME"
